f2a8698a01f8559225cc1e491303f45b97f04e6d
ff6347 <hash@ff6347.email>
2022-05-30 15:13:18 +0200

    refactor

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-// prettier default configuration
-// https://prettier.io/docs/en/options.html
-
-module.exports = {
-  trailingComma: "all",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-};
